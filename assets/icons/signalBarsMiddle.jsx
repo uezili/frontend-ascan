2697353634063f74e3b2cb12f1a5bcade717e017
Uezili <weslmaciel@gmail.com>
2023-09-20 23:06:16 -0300

    New icons.

---
+++ assets/icons/signalBarsMiddle.jsx
@@ -0,0 +1,14 @@
+export const SignalBarsMiddle = () => {
+	return (
+		<svg
+			fill="#F8DE22"
+			id="Layer_1"
+			data-name="Layer 1"
+			viewBox="0 0 24 24"
+			width="512"
+			height="512"
+		>
+			<path d="m11,11.5v12.5h-4v-12.5c0-.827.673-1.5,1.5-1.5h1c.827,0,1.5.673,1.5,1.5Zm-7.5,5.5h-1c-.827,0-1.5.673-1.5,1.5v5.5h4v-5.5c0-.827-.673-1.5-1.5-1.5Z" />
+		</svg>
+	);
+};
