0a6bf63cee250675ae33c7c2df13205a5b14cfb3
Uezili <weslmaciel@gmail.com>
2023-09-21 13:23:46 -0300

    Remove navbar.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,12 +8,6 @@ export const Header = () => {
 					<img src="assets/ia-logo.svg" width="70px" />
 					<h1>To-Do List App</h1>
 				</div>
-				<nav>
-					<ul>
-						<li>Home</li>
-						<li>About</li>
-					</ul>
-				</nav>
 			</div>
 		</header>
 	);
