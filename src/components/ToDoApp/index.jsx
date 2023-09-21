72ed737dfbe0ea91521845542c0203c05845a3f2
Uezili <weslmaciel@gmail.com>
2023-09-20 23:29:37 -0300

    Adding todo list in todoApp component.

--- src/components/ToDoApp/index.jsx
+++ src/components/ToDoApp/index.jsx
@@ -1,12 +1,20 @@
+import { useSelector } from 'react-redux';
 import { Header } from '../Header';
 import { ToDoForm } from '../ToDoForm';
+import { ToDoList } from '../ToDoList';
+
 import './styles.css';
 
 export const ToDoApp = () => {
+	const todos = useSelector((state) => state.todos);
+
 	return (
 		<div>
 			<Header />
-			<ToDoForm />
+			<div className="grid grid-cols-2 w-full">
+				<ToDoForm />
+				<ToDoList todos={todos} />
+			</div>
 		</div>
 	);
 };
