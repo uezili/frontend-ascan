c98fbdd369d80d1c8f3ea50fda0b87f9bc2b6cdf
Uezili <weslmaciel@gmail.com>
2023-09-22 14:35:56 -0300

    Close modal after submit.

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -5,18 +5,19 @@ import { Plus } from '../../../assets/icons/plus';
 
 export const Modal = () => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [rotate, setRotate] = useState(0);
+	const [rotate, setRotate] = useState('');
 
 	const openModal = () => {
 		if (isOpen == true) {
 			setIsOpen(false);
 			setRotate('rotate-0');
+			() => openModal;
 		} else {
 			setIsOpen(true);
 			setRotate('rotate-45');
 		}
 	};
-
+	console.log(isOpen);
 	return (
 		<div className="flex justify-center ">
 			<button className="open-modal" onClick={openModal}>
@@ -26,7 +27,7 @@ export const Modal = () => {
 			{isOpen && (
 				<div className="modal">
 					<div className="modal-content">
-						<ToDoForm />
+						<ToDoForm onAfterSubmit={() => setIsOpen(false)} />
 					</div>
 				</div>
 			)}
