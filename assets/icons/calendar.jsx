2ed1967520773fa9118c3f73b1289234900d5918
Uezili <weslmaciel@gmail.com>
2023-10-11 21:49:13 -0300

    New prop for icon.

--- assets/icons/calendar.jsx
+++ assets/icons/calendar.jsx
@@ -1,6 +1,7 @@
-export const Calendar = () => {
+export const Calendar = ({ className }) => {
 	return (
 		<svg
+			className={className}
 			xmlns="http://www.w3.org/2000/svg"
 			id="Outline"
 			viewBox="0 0 24 24"
